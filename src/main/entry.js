41ecd958199ef9a01605f72d6895e77a87a2ce27
Inna Denysiuk <innadenysiuk275@gmail.com>
2025-06-04 08:39:58 +0300

    Set up keydown event listener

--- src/main/entry.js
+++ src/main/entry.js
@@ -2,3 +2,9 @@ import { Grid } from '../core/grid.js';
 import { GRID_SIZE } from '../constants.js';
 
 const grid = new Grid(GRID_SIZE);
+
+const handleKeyDown = (event) => {
+
+};
+
+document.addEventListener('keydown', handleKeyDown);
